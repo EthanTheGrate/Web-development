a9426666b362eb6181825a66cce9526bbfeee701
EthanTheGrate <amogussussy1221@outlook.com>
2023-08-24 20:04:33 -0600

    gayse

--- Password Generator/basic.py
+++ Password Generator/basic.py
@@ -1,5 +1,25 @@
-isUppercase = False
-isNumbers = False
-isSymbols = False
+isuppercase = bool(input("With uppercase letters?"))
+isnumbers = bool(input("With numbers?"))
+issymbols = bool(input("With symbols?"))
+length = int(input("How many characters do you want?"))
 
-isUppercase = bool(input("With uppercase letters?"))
+print(isuppercase)
+print(isnumbers)
+print(issymbols)
+import random
+def generate_password():
+    choices = list("abcdefghijkmlopqrstuvxyz")
+
+    if isuppercase == True:
+        choices.extend("ABCDEFGHIJKLMOPQRSTUVWXYZ")
+    if isnumbers == True:
+        choices.extend("0123456789")
+    if issymbols == True:
+        choices.extend('''"'[]|\~!@#{}$/%^&*()_-+=<>,.:;?''')
+
+    mypassword = ""
+    for i in range(length):
+        mypassword += random.choice(choices)
+
+    print(mypassword)
+generate_password()
