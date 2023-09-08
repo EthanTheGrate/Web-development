fc62d8d8673de33b15bf666cdcffa485e9ca8494
EthanTheGrate <amogussussy1221@outlook.com>
2023-09-07 20:16:33 -0600

    aku sayang kamu juga sayang

--- Password Generator/basic.py
+++ Password Generator/basic.py
@@ -1,31 +1,5 @@
-# isuppercase = bool(input("With uppercase letters?"))
-# isnumbers = bool(input("With numbers?"))
-# issymbols = bool(input("With symbols?"))
-# length = int(input("How many characters do you want?"))
-
-# print(isuppercase)
-# print(isnumbers)
-# print(issymbols)
-# import random
-# def generate_password():
-#     choices = list("abcdefghijkmlopqrstuvxyz")
-
-#     if isuppercase == True:
-#         choices.extend("ABCDEFGHIJKLMOPQRSTUVWXYZ")
-#     if isnumbers == True:
-#         choices.extend("0123456789")
-#     if issymbols == True:
-#         choices.extend('''"'[]|\~!@#{}$/%^&*()_-+=<>,.:;?''')
-
-#     mypassword = ""
-#     for i in range(length):
-#         mypassword += random.choice(choices)
-
-#     print(mypassword)
-# generate_password()
-
 import random 
-from flask import render_template, Flask 
+from flask import render_template, Flask, request  
 
 app = Flask(__name__)
 
@@ -37,6 +11,29 @@ def index():
 def fill_form():
     return render_template('fill_form.html')
 
+@app.route('/password')
+def password():
+    choices = list("abcdefghijklmnopqrstuvwxyz")
+
+    isUppercase = request.args.get("uppercase")
+    isNumbers = request.args.get("numbers")
+    isSymbols = request.args.get("symbols")
+    length = request.args.get("length")
+
+    if int(length) == None: 
+        length = 8 
+    if isUppercase == '1': 
+        choices.extend("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    if isNumbers == '1':
+        choices.extend("0123456789")
+    if isSymbols == '1':
+        choices.extend('''~!@#$%^&*()_+-=){}[]\|:;"',.<>?/]''')
+
+    mypassword = ""
+    for i in range(int(length)):
+        mypassword += random.choice(choices)
+
+    return render_template('password.html', mypassword=mypassword)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
