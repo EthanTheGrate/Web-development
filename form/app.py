802787a32c6b6ea862a2f698b89f9fc370295ba8
EthanTheGrate <amogussussy1221@outlook.com>
2023-09-23 12:16:57 -0600

    gajaefwiohagrihoihoafs

---
+++ form/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+messages=[]
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["message"]
+
+    messages.append({"name": name, "email": email, "message": message})
+
+    return redirect(url_for("thank_you"))
+
+@app.route('/thank_you')
+def thank_you():
+    return "Celina is a loser"
+
+if __name__ == '__main__':
+    app.run()
