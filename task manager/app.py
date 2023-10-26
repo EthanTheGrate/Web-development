b6c15d2685513f3003e24f3630ac0fe41e723464
EthanTheGrate <amogussussy1221@outlook.com>
2023-10-25 19:32:35 -0600

    child p

--- task manager/app.py
+++ task manager/app.py
@@ -1,10 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
+from flask_login import UserMixin, login_user, logout_user, login_required, LoginManager
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(30)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///user_manager.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 @app.route('/')
 def index():
     return "Hello world"
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(max=80)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(max=80)])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Registration successful! You can now login")
+        return redirect('login')
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            flash("yay you logged in successfully")
+            return redirect(url_for('index'))
+        else:
+            flash("womp womp you failed to login")
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("your logout was successful :cry:")
+    return redirect("index")
 
 if __name__ == '__main__':
     app.run(debug=True)
