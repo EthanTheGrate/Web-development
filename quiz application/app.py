b5f673889a1fbcede8e4aa993238e77e4b0bac55
EthanTheGrate <amogussussy1221@outlook.com>
2023-10-28 10:12:57 -0600

    rape time

---
+++ quiz application/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+quiz_question = "what is the capital of france?"
+quiz_options = ["London", "Berlin", "Paris", "Delhi"]
+correct_answer = "Paris"
+
+@app.route('/')
+def index():
+    return "hi welcome to quiz application"
+
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+    if request.method == "POST":
+        user_answer = request.form.get('answer')
+        if user_answer == correct_answer:
+            return "correct! paris is the capital of france"
+        else:
+            return "incorrect! try again!"
+    return render_template('quiz.html', question=quiz_options, options=quiz_options)
+
+if __name__ == '__main__':
+    app.run(debug=True)
